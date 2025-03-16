416e90e431d1da7298f4056ae7165a311d10bc5e
Rishmo <sweetmoonsahu@gmail.com>
2025-03-16 14:48:07 +0530

    added page.module.css page for styling

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 
+import styles from './page.module.css'
 export default function Home() {
+
+  console.log("Where do i render?"); //server side rendering
   return (
-      <main className="flex min-h-screen flex-col text-2xl items-center justify-center font-bold p-20">
+      // <main className="flex min-h-screen flex-col text-2xl items-center justify-center font-bold p-20">
+      //   Hello World
+      // </main>
+      <main className= {styles.main}>
         Hello World
       </main>
       
